3d688c2e232dc04ddfab9fb3006d2fd7ad03befc
NicoMaker <nicola.marano02gmail,com>
2025-02-12 09:24:02 +0100

    reame code of py file

--- Esercizi/python-app-starting-setup/rng.py
+++ Esercizi/python-app-starting-setup/rng.py
@@ -3,9 +3,10 @@ from random import randint
 min_number = int(input('Please enter the min number: '))
 max_number = int(input('Please enter the max number: '))
 
-if (max_number < min_number): 
-  print('Invalid input - shutting down...')
-else:
-  rnd_number = randint(min_number, max_number)
-  print(rnd_number)
+# Inversione se necessario
+if max_number < min_number:
+    min_number, max_number = max_number, min_number
 
+# Generazione del numero casuale
+rnd_number = randint(min_number, max_number)
+print(rnd_number)
